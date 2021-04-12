45d4d7fd1b0bb118c9db955391523e55384ae834
Fernando Azevedo <fernando.azevedo.peres@gmail.com>
2021-04-12 16:51:24 -0300

    Comentando e tirando print de RTT

--- src/RTT.c
+++ src/RTT.c
@@ -5,88 +5,127 @@
 #include "RTT.h"
 #include "Grafo.h"
 #include <limits.h>
-#include "Corzinha.h"
 
 #define INF INT_MAX
 
-
 struct atalhos{
-
     double** mat;
-    int ncol, nlin;
-
+    int nlin;
 };
 
 // Inicializa um struct atalhos que basicamente é
-// uma matriz de doubles na qual mat[a][b] é a menor distãncia
+// uma matriz degenerada de doubles na qual mat[a][b] é a menor distãncia
 // entre um vértice a com um vértice b.
+// Na matriz, só existiram as linhas em que mat[a] seja um Servidor, Cliente ou Monitor.
 Atalhos * inicializaAtalhos(Grafo* g){
 
     Atalhos * novo = malloc(sizeof(*novo));
-    //novo->nlin = retornaQtdCliente(g) + retornaQtdServidor(g) + retornaQtdMonitor(g);
+
     novo->nlin = retornaNumVertices(g);
-    novo->ncol = retornaNumVertices(g);
+
     novo->mat = malloc(sizeof(double*) * novo->nlin);
+
     // Inicializa todas as linha como NULL
     for (int i=0; i < novo->nlin; i++) novo->mat[i] = NULL;
+
     return novo;
 }
 
-// Insere um vetor de doubles já alocados nos atalhos
+// Insere um vetor de doubles já alocados nos atalhos.
 void insereLinhaAtalho(Atalhos* at, double* vet, int i){
     at->mat[i] = vet;
 }
 
-// Retorna o RTT padrão entre o vértice p e o vértice q
+// Retorna o RTT real entre o vértice p e o vértice q.
 double RTT(Atalhos * a, int p, int  q){
     return a->mat[p][q] + a->mat[q][p];
 }
 
+//Função para calcular o RTT_Relativo.
 double RTT_Relativo(Atalhos* a, int p, int q, Grafo* grafo){
 
+    //Seta o minimo como infinito para pegar o menor valor.
     double minimo = INF;
 
     for(int i = 0; i < retornaQtdMonitor(grafo); i++) {
         int j = retornaMonitor(grafo, i);
+        //Calcula um RTT relativo passando por um monitor.
         double relativo = (RTT(a, p, j) + RTT(a, j, q));
+        //Atribui à minimo o valor do minimo entre minimo e o novo RTT_Relativo.
         if (minimo > relativo) minimo = relativo;
     }
 
     return minimo;
 }
 
+//Função para calcular o RTT_FINAL.
 double RTT_Final(Atalhos * at, int p, int q, Grafo * g){
 
-    return (retornaQtdMonitor(g)? RTT_Relativo(at, p, q, g) : RTT(at, p, q)) / RTT(at, p, q);
-}
-
-void imprimeAtalhos(Atalhos * at){
-
-    printf(NORMAL MAGENTA "Atalhos:" RESET "\n");
-    for (int i=0; i < at->nlin; i++) {
-        if(at->mat[i] != NULL) {
-            for (int j = 0; j < at->ncol; j++) printf("%.1lf ", at->mat[i][j]);
-            printf("\n");
-        }
-    }
+    return RTT_Relativo(at, p, q, g) / RTT(at, p, q);
 
 }
 
+//Função para liberar nossa matriz degenerada de distâncias mínimas.
 void liberaAtalhos(Atalhos * at){
 
+    //Se a posição da matriz não é NULL libera a linha.
     for (int i=0; i < at->nlin; i++) if (at->mat[i]) free(at->mat[i]);
     free(at->mat);
     free(at);
 }
 
+//Struct para armazenar as informações de um RTT para fazer a ordenação.
+typedef struct rtt{
+  int orig;
+  int dest;
+  double info;
+}infoRTT;
+
+//Função auxiliar para ordenar a estrutura de RTT's.
+int compRTT(const void *a, const void *b){
+    infoRTT const* A = a;
+    infoRTT const* B = b;
+
+    //Compara o RTT.
+    if((*A).info > (*B).info) return 1;
+    //Se for igual compara a origem.
+    else if((*A).info == (*B).info){
+        if((*A).orig > (*B).orig)
+            return 1;
+        //Se for igual compara o destino.
+        else if((*A).orig == (*B).orig) {
+            if ((*A).dest > (*B).dest)
+                return 1;
+        }
+        return 0;
+    }
+    else return -1;
+}
+
+//Função para calcular e imprimir os RTT's finais ordenados.
 void calculaTodosRTTFinal(Atalhos * at, Grafo * grafo){
 
-    //Calcula rtt real
-    double RTT_FINAL;
+    //Cria um vetor de tamanho |S|*|C| de infoRTT.
+    infoRTT* listaRTT = malloc(sizeof(infoRTT) * (retornaQtdServidor(grafo) * retornaQtdCliente(grafo)));
+
+    //Calcula rtt real.
+    int k = 0;
     for(int i = 0; i < retornaQtdServidor(grafo); i++){
         for(int j = 0; j < retornaQtdCliente(grafo); j++) {
-            RTT_FINAL = RTT_Final(at, retornaServidor(grafo,i), retornaCliente(grafo,j), grafo);
-            printf("%d %d %.16lf\n", retornaServidor(grafo,i), retornaCliente(grafo,j), RTT_FINAL);
+            listaRTT[k].orig = retornaServidor(grafo,i);
+            listaRTT[k].dest = retornaCliente(grafo,j);
+            listaRTT[k++].info = RTT_Final(at, retornaServidor(grafo,i), retornaCliente(grafo,j), grafo);
         }
     }
+
+    //Ordena o vetor de RTT's.
+    qsort(listaRTT, k, sizeof(listaRTT[0]), compRTT);
+
+    //Imprime os RTT's ordenados.
+    for(int i=0; i<k; i++) {
+        printf("%d %d %.16lf\n", listaRTT[i].orig, listaRTT[i].dest, listaRTT[i].info);
+    }
+
+    //Libera os RTT's.
+    free(listaRTT);
 }
--- src/RTT.h
+++ src/RTT.h
@@ -11,11 +11,10 @@ typedef struct atalhos Atalhos;
 
 Atalhos * inicializaAtalhos(Grafo* g);
 void insereLinhaAtalho(Atalhos* at, double* vet, int i);
-double RTT(Atalhos * a, int p, int  q);
-double RTT_Relativo(Atalhos* a, int p, int q, Grafo* grafo);
-double RTT_Final(Atalhos * at, int p, int q, Grafo * g);
-void imprimeAtalhos(Atalhos * at);
 void liberaAtalhos(Atalhos * at);
 void calculaTodosRTTFinal(Atalhos * at, Grafo * grafo);
+double RTT(Atalhos * a, int p, int  q);
+double RTT_Final(Atalhos * at, int p, int q, Grafo * g);
+double RTT_Relativo(Atalhos* a, int p, int q, Grafo* grafo);
 
 #endif //TRABED2___2020_2_RTT_H
